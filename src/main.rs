d5eeb0dd8da8c843c0cb2f9a2a734e8957990241
Bryan Gilbert <gilbertw1@gmail.com>
2018-09-06 06:54:48 -0400

    Formatting updates

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,16 @@ use domain::resolv::lookup::addr::lookup_addr;
 
 fn main() {
     let ip_addr = "89.242.204.127".parse::<IpAddr>().unwrap();
-
+    
     // Use Naive Solution
     let hostnames = lookup_hostnames(ip_addr);
-    println!("Reverse Ip Look Results For: {}", ip_addr);
+    println!("[NAIVE] Reverse Ip Look Results For: {}", ip_addr);
     for hostname in hostnames {
         println!(" - {}", hostname);
     }
 
     // Use Actual Solution
+    println!("[ACTUAL] Reverse Ip Look Results For: {}", ip_addr);
     let handle = create_lookup_handle();
     let result_future = handle.lookup_hostnames(ip_addr);
     for hostname in result_future.wait().unwrap() {
@@ -53,34 +54,28 @@ fn create_lookup_handle() -> DnsLookupHandle {
     thread::spawn(move || {
         let mut core = Core::new().unwrap();
         let core_handle = core.handle();
-        let resolv = Resolver::new(&core.handle());
-        let resolver_loop = req_rx.map_err(|e| println!("error = {:?}", e)).for_each(
-            move |request| {
-                let future = handle_reverse_lookup(request, resolv.clone());
-                core_handle.spawn(future);
-                Ok(())
-            },
-        );
+        let resolv = Resolver::new(&core.handle()); 
+        let resolver_loop =
+            req_rx.map_err(|e| println!("error = {:?}", e))
+                  .for_each(move |request| {
+                      let future = handle_reverse_lookup(request, resolv.clone());
+                      core_handle.spawn(future);
+                      Ok(())
+                  });
 
-        core.run(resolver_loop).expect(
-            "[dns] Failed to start reactor core loop.",
-        );
+        core.run(resolver_loop).expect("[dns] Failed to start reactor core loop.");
     });
 
     DnsLookupHandle { request_sender: req_tx }
 }
 
-fn handle_reverse_lookup(
-    request: ReverseLookupRequest,
-    resolv: Resolver,
-) -> impl Future<Item = (), Error = ()> {
+fn handle_reverse_lookup(request: ReverseLookupRequest, resolv: Resolver) -> impl Future<Item=(), Error=()> {
     lookup_addr(resolv, request.ip).then(|result| {
-        let response = match result {
-            Ok(addrs) => ReverseLookupResponse {
-                names: addrs.iter().map(|n| n.to_string()).collect(),
-            },
-            Err(_) => ReverseLookupResponse { names: Vec::new() },
-        };
+        let response = 
+            match result {
+                Ok(addrs) => ReverseLookupResponse { names: addrs.iter().map(|n| n.to_string()).collect() },
+                Err(_) => ReverseLookupResponse { names: Vec::new() },
+            };
         request.sender.send(response);
         Ok(())
     })
@@ -88,28 +83,22 @@ fn handle_reverse_lookup(
 
 struct ReverseLookupRequest {
     ip: IpAddr,
-    sender: oneshot::Sender<ReverseLookupResponse>,
+    sender: oneshot::Sender<ReverseLookupResponse>
 }
 
 struct ReverseLookupResponse {
-    names: Vec<String>,
+    names: Vec<String>
 }
-
+ 
 #[derive(Clone)]
 pub struct DnsLookupHandle {
     request_sender: mpsc::UnboundedSender<ReverseLookupRequest>,
 }
 
 impl DnsLookupHandle {
-    pub fn lookup_hostnames(
-        &self,
-        ip: IpAddr,
-    ) -> impl Future<Item = Vec<String>, Error = oneshot::Canceled> {
+    pub fn lookup_hostnames(&self, ip: IpAddr) -> impl Future<Item=Vec<String>, Error=oneshot::Canceled> {
         let (resp_tx, resp_rx) = oneshot::channel::<ReverseLookupResponse>();
-        let result = self.request_sender.unbounded_send(ReverseLookupRequest {
-            ip: ip,
-            sender: resp_tx,
-        });
+        let result = self.request_sender.unbounded_send(ReverseLookupRequest { ip: ip, sender: resp_tx });
         resp_rx.map(|res| res.names)
     }
 }
